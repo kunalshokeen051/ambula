5fc7980673b9aa7bdf935f7945b8d9a145e9f01b
kunalshokeen051 <119399583+kunalshokeen051@users.noreply.github.com>
2023-07-17 11:12:47 +0530

    page transitions added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,53 @@
 import '@/styles/globals.css'
 import Header from '@/Components/Header';
 import Footer from '@/Components/Footer';
+import { motion } from 'framer-motion';
 
 export default function App({ Component, pageProps }) {
   return (
-  <>
-  <Header/>
-  <Component {...pageProps} />
-  <Footer/>
-  </>
+    <PageTransition>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </PageTransition>
   )
 }
+
+const PageTransition = ({ children }) => {
+  const pageVariants = {
+    initial: {
+      opacity: 0,
+      scale: 0.9,
+      y: 50,
+    },
+    animate: {
+      opacity: 1,
+      scale: 1,
+      y: 0,
+      transition: {
+        duration:1,
+        ease: 'easeInOut',
+      },
+    },
+    exit: {
+      opacity: 0,
+      scale: 0.9,
+      y: -50,
+      transition: {
+        duration: 1,
+        ease: 'easeInOut',
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={pageVariants}
+    >
+      {children}
+    </motion.div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import Image from 'next/image'
+
 
 export default function Home() {
 
@@ -7,7 +7,7 @@ const router = useRouter();
 
   return (
     <div className="w-full min-h-screen">
-      <div className="w-full min-h-[calc(100vh-80px)] md:px-20 px-4 flex md:flex-row flex-col-reverse">
+    <div className="w-full min-h-[calc(100vh-80px)] md:px-20 px-4 flex md:flex-row flex-col-reverse">
         {/* Left Container start */}
         <div className="md:flex-1 flex flex-col gap-4 justify-center py-10">
         <h1 className='md:text-5xl text-3xl leading-snug justify-center'>
